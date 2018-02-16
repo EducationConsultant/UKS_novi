a2363cff4686fb4b647c4b9a259844c6df9c2672
Svetlana Djuric <cdjuric963@gmail.com>
2018-02-16 11:07:28 +0100

    Ispravljena putanja

--- ProjekatUKS/ProjekatUKS/urls.py
+++ ProjekatUKS/ProjekatUKS/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('github/', include('github.urls')),
+    path('boohub/', include('github.urls')),
     path('admin/', admin.site.urls),
 ]
