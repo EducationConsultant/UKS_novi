be10ccf5d5bce86ea0a87396b51108d378c20bca
Milena Lalic <mlalic994@gmail.com>
2018-02-21 16:35:07 +0100

    Dodat test #36

--- ProjekatUKS/github/tests.py
+++ ProjekatUKS/github/tests.py
@@ -9,3 +9,11 @@ class MyTest(unittest.TestCase):
     def test_dummy(self):
         self.assertEqual(3, 4)
 
+    def test_list(self):
+        print('**************************************************')
+
+        testList = [1, 2, 3]
+        value = [4,5,6]
+
+        self.assertListEqual(testList,value)
+
