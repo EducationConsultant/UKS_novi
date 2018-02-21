633823da9d02efe39caa26364a51e84f983cf28d
Milena Lalic <mlalic994@gmail.com>
2018-02-21 17:27:47 +0100

    Vracen test za user-a #36

--- ProjekatUKS/github/tests.py
+++ ProjekatUKS/github/tests.py
@@ -5,15 +5,40 @@ import unittest
 #from . import models
 
 
-class MyTest(unittest.TestCase):
-    def test_dummy(self):
-        self.assertEqual(3, 4)
+class UserTestCase(unittest.TestCase):
+    def setUp(self):
+        self.user = User.objects.create(firstname="Djole", lastname="Pingvin", username="pingvin_djole",
+                            password="djole123", email="djole@gmail.com",
+                            isActive=True, loggedin=False)
 
-    def test_list(self):
-        print('**************************************************')
+    def test_login_user(self):
+        self.user.loggedin = True
+        self.user.save()
 
-        testList = [1, 2, 3]
-        value = [4,5,6]
+        self.assertTrue(self.user.loggedin)
 
-        self.assertListEqual(testList,value)
+    def test_logout(self):
+        self.user.loggedin = False
+        self.user.save()
 
+        self.assertFalse(self.user.loggedin)
+
+    def test_change_username(self):
+        new_username = "new_username"
+
+        self.user.username = new_username
+        self.user.save()
+
+        self.assertEqual(self.user.username, "new_username")
+
+    def test_change_password(self):
+        old_password = "djole123"
+        self.assertEqual(self.user.password, old_password)
+
+        new_password = "newpass123"
+        confirm_new_password = "newpass123"
+        self.assertEqual(new_password, confirm_new_password)
+
+        self.user.password = new_password
+        self.user.save()
+        self.assertEqual(self.user.password, new_password)
