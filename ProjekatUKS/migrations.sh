7784f953bf9c1134ab873a1a57ac111730882f00
Svetlana Djuric <cdjuric963@gmail.com>
2018-02-23 19:52:43 +0100

    Vraceno sve na master pa onda deploy.

--- ProjekatUKS/migrations.sh
+++ ProjekatUKS/migrations.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sleep 10
+sleep 15
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8027
 exec "$@"
